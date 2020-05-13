1f9d67c6b913939683ce88f72a9a2752c74cab08
cjamcu <cjamcu@gmail.com>
2020-05-12 19:41:23 -0500

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/cjamcu/Develop/Tools/flutter-sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/cjamcu/Develop/Personal/Aprendizaje/Flutter/diariodecuba_flutter/lib/packages/webview_flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/cjamcu/Develop/Tools/flutter-sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.3.19"
+export "FLUTTER_BUILD_NUMBER=9"
